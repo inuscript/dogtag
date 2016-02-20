5076902d3412dc6ee5748e0031c59d861ba338a5
inuscript <inuscript@gmail.com>
2016-02-20 20:38:40 +0900

    to es5

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-"use strict"
 var axios = require("axios")
 var url = "https://raw.githubusercontent.com/inuscript/dogtag/master/tags.txt"
 
 module.exports = function tags(){
-  return axios(url).then(res => {
+  return axios(url).then(function(res){
     return res.data.split("\n").filter( t => {
       return t.length > 0
     })
